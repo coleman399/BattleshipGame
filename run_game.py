56072bd818eaa231396888b21289545ce5c7a909
Dillon Coleman <coleman399@gmail.com>
2021-10-19 11:00:03 -0700

    finished inital ship placement for pvp game

--- run_game.py
+++ run_game.py
@@ -13,7 +13,7 @@ class RunGame:
     def run_game(self):
         self.welcome()
         self.display_rules()
-        self.display_board()
+        self.display_board(self.player_one)
         game_mode = self.choose_game_mode()
         if game_mode == True:
             self.player_vs_player()
@@ -23,9 +23,13 @@ class RunGame:
     def welcome(self):
         print("--- welcome to battleship! ---")
 
-    def display_board(self):
-        self.game_board.print_gameboard()
-
+    def display_board(self, player):
+        try:
+            if player.name == player.name:
+                print(f"                              --- PLAYER {player.name}'s GAME BOARD ---")
+                player.game_board.print_gameboard()
+        except:
+            self.game_board.print_gameboard()
     def display_rules(self):
         pass
 
@@ -48,59 +52,69 @@ class RunGame:
         self.player_two = Human(input("name of player two: "))
         # creating each player's fleet
         self.player_one.fleet_list = self.create_fleet(self.player_one)
+        self.display_board(self.player_one)
         self.player_two.fleet_list = self.create_fleet(self.player_two)
-
-        self.player_one.game_board.print_gameboard()
+        self.display_board(self.player_two)
 
     def player_vs_ai(self):
         self.player_one = Human(input("name of player one: "))
         self.player_one.fleet_list = self.create_fleet(self.player_one)
         self.computer.fleet_list = self.create_fleet(self.computer)
 
+    # When ships are placed on the board horizontally the are placed at the desired (x, y) coordinate then "grow" to the left.
+    #                    0   1   2   3   4
+    # example: (0, 4) = [ ]  <   +   +   >
+    # while ships are placed vertically the ships "grows" down
+    # example: (0, 0) = 0  1   2   3
+    #                 0 ^ [ ] [ ] [ ]
+    #                 1 + [ ] [ ] [ ]
+    #                 2 + [ ] [ ] [ ] 
+    #                 3 v [ ] [ ] [ ]
+
     def create_fleet(self, player):
         for ship in player.fleet_list:
             occupied = True
-            is_vertical = None
             set_ship = None
             while(occupied):
                 occupied = False
-                try:# ToDo: make sure you fix input errors for row and column
-                    row = int(input(f"{player.name}: what row would you like to place your {ship.name}? "))
-                    column = int(input(f"{player.name}: what column would you like to place your {ship.name}? "))
-                except:
-                    print("Please use numbers 0 through 9 as inputs.")    
-                self.set_vertical(ship, player)
+                coordinates = (self.check_coordinates(player, ship))
+                self.set_vertical(player, ship)
                 if ship.is_vertical == True:
                     for p in range(ship.ship_length):
-                        if not self.is_ocean(row + p, column, player.board):
-                            player.fleet_list.append(ship.set_location(row, column, ship.is_vertical))
+                        if not self.is_ocean(coordinates[0] + p, coordinates[1], player.board):
+                            player.fleet_list.append(ship.set_location(coordinates[0], coordinates[1], ship.is_vertical))
                             occupied = True
+                            continue
                 else:
                     for p in range(ship.ship_length):
-                        if not self.is_ocean(row, column - p, player.board):
+                        if not self.is_ocean(coordinates[0], coordinates[1] - p, player.board):
                             occupied = True
+                            continue
+                if(occupied): 
+                    print("Please select another location.")
             if ship.is_vertical == True:
-                player.board[row][column] = " ^ "
-                player.board[row + ship.ship_length-1][column] = " v "
+                player.board[coordinates[0]][coordinates[1]] = " ^ "
+                player.board[coordinates[0] + ship.ship_length-1][coordinates[1]] = " v "
                 if set_ship != None:
-                    player.number_board[row][column] = set_ship
-                    player.number_board[row + ship.ship_length - 1][column] = set_ship
+                    player.number_board[coordinates[0]][coordinates[1]] = set_ship
+                    player.number_board[coordinates[0] + ship.ship_length - 1][coordinates[1]] = set_ship
                 for p in range(ship.ship_length - 2):
-                    player.board[row + p + 1][column] = " + "
+                    player.board[coordinates[0] + p + 1][coordinates[1]] = " + "
                     if set_ship != None:
-                        player.number_board[row + p + 1][column] = set_ship
+                        player.number_board[coordinates[0] + p + 1][coordinates[1]] = set_ship
             else:
-                player.board[row][column] = " > "
-                player.board[row][column - ship.ship_length + 1] = " < "
+                player.board[coordinates[0]][coordinates[1]] = " > "
+                player.board[coordinates[0]][coordinates[1] - ship.ship_length + 1] = " < "
                 if set_ship != None:
-                    player.number_board[row][column] = set_ship
-                    player.number_board[row][column - ship.ship_length + 1] = set_ship
+                    player.number_board[coordinates[0]][coordinates[1]] = set_ship
+                    player.number_board[coordinates[0]][coordinates[1] - ship.ship_length + 1] = set_ship
                 for p in range(ship.ship_length - 2):
-                    player.board[row][column - p - 1] = " + "
+                    player.board[coordinates[0]][coordinates[1] - p - 1] = " + "
                     if set_ship != None:
-                        player.number_board[row][column - p - 1] = set_ship
+                        player.number_board[coordinates[0]][coordinates[1] - p - 1] = set_ship
+            self.display_board(player)
 
-    def set_vertical(self, ship, player):
+    def set_vertical(self, player, ship):
         loop = True
         while loop is True:
             answer = input(f"{player.name}: would you like {ship.name} placed vertically? ")
@@ -124,3 +138,13 @@ class RunGame:
             return 1
         else:
             return 0
+
+    def check_coordinates(self, player, ship):
+        # TODO: inform user of ship length when asking for coordinates
+        try:
+            row = int(input(f"{player.name}: what row would you like to place your {ship.name}? "))
+            column = int(input(f"{player.name}: what column would you like to place your {ship.name}? "))
+            return row, column
+        except:
+            print("Please use numbers 0 through 9 as inputs.")
+            self.check_coordinates(player, ship) 
